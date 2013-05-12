fc2d9f6dfcc2c1fff3f4e2f5a862a601049a1226
David <ddk128@gmail.com>
2013-05-12 23:31:14 +0200

    Fix config paths.

--- src/test/scala/assets/test/markup.scala
+++ src/test/scala/assets/test/markup.scala
@@ -1,16 +1,29 @@
 package assets.test
 
+import java.io._
+import java.util._
+
+import scala.util.control.Breaks._
+import scala.collection.JavaConversions._
+
 import org.junit._
 import org.scalatest.junit._
 import assets._
 
 class Markup extends AssertionsForJUnit {
   val coffee = new Coffee(new Module())
+  val less = new Less()
 
   val module = new Module()
   module.evaluateString(coffee.compile("assets/markup.coffee"))
 
-  val markup = new assets.Markup(module,"modules/main.coffee")
+  val markup = new assets.Markup(new Module(module),
+    new {
+      val stylePaths:List[String] = "assets/styles"::Nil;
+      val scriptPaths:List[String]  = "assets/scripts"::Nil;
+      val includePaths:List[String]  = "modules"::Nil;
+      val outputPath:String = "assets"},
+    "main",coffee,less)
 
   @Test
   def initialize() {
@@ -21,6 +34,6 @@ class Markup extends AssertionsForJUnit {
 
   @Test
   def render() {
-    System.out.println("render: "+markup.render())
+    System.out.println("render: "+markup.markup)
   }
 }
