33e3ea3df97cb8f4b2e925208d8f3d31c0406d17
David <ddk128@gmail.com>
2013-05-12 12:02:36 +0200

    Remove rhino code from entry point.

--- src/main/groovy/assets/assets.groovy
+++ src/main/groovy/assets/assets.groovy
@@ -4,50 +4,10 @@ import org.gradle.api.*
 import org.gradle.api.plugins.*
 import org.mozilla.javascript.*
 
-/*
- * val ctx = Context.enter()
-
-    try {
-      val scope = context.newScope(ctx)
-
-      context.getScope().get("CoffeeScript",context.getScope()) match {
-        case coffee:Scriptable => {
-          coffee.get("compile",coffee) match {
-            case f:Function => {
-              f.call(ctx,scope,coffee,Array(source)).toString()
-            }
-          }
-        }
-      }
-    } finally {
-      Context.exit()
-    }
-  }
- */
 
 class AssetsPlugin implements Plugin<Project> {
-  ScriptableObject scope;
-
-  // AssetsPlugin() {
-    /*def ctx = Context.enter()
-
-    def scope = ctx.initStandardObjects()
-    def jsStdout = Context.javaToJS(System.out,scope)
-    ScriptableObject.putProperty(scope,"stdout",jsStdout)
-    def jsStderr = Context.javaToJS(System.err,scope)
-    ScriptableObject.putProperty(scope,"stderr",jsStderr)
-    def jsSystem = Context.javaToJS(new System(),scope)
-    ScriptableObject.putProperty(scope,"system",jsSystem)*/
-
-    /*ctx.evaluateReader(scope,
-      new InputStreamReader(this.getClass().getClassLoader()
-        .getResourceAsStream("src/resources/coffee-script.js")),
-      "coffee",0,null)*/
-
-    // this.scope = scope
-
-    // Context.exit()
-  // }
+  Coffee coffee = new Coffee()
+  Less less = new Less()
 
   void apply(Project project) {
     project.extensions.create("assets", AssetsExtension)
@@ -56,8 +16,6 @@ class AssetsPlugin implements Plugin<Project> {
       println("assets: "+project.assets.src)
     }
   }
-
-
 }
 
 class AssetsExtension {
