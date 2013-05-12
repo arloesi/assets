bf98e39d214db89b122a1dd317790478dc356ee1
David <ddk128@gmail.com>
2013-05-12 07:45:55 +0200

    Add module unit test.

--- src/main/groovy/assets/module.groovy
+++ src/main/groovy/assets/module.groovy
@@ -3,24 +3,60 @@ package assets
 import org.mozilla.javascript.*
 
 class Module {
+  ScriptableObject scope;
+
   Module() {
-  /*def ctx = Context.enter()
+    def ctx = Context.enter()
 
-  def scope = ctx.initStandardObjects()
-  def jsStdout = Context.javaToJS(System.out,scope)
-  ScriptableObject.putProperty(scope,"stdout",jsStdout)
-  def jsStderr = Context.javaToJS(System.err,scope)
-  ScriptableObject.putProperty(scope,"stderr",jsStderr)
-  def jsSystem = Context.javaToJS(new System(),scope)
-  ScriptableObject.putProperty(scope,"system",jsSystem)*/
+    def scope = ctx.initStandardObjects()
+    def jsStdout = Context.javaToJS(System.out,scope)
+    ScriptableObject.putProperty(scope,"stdout",jsStdout)
+    def jsStderr = Context.javaToJS(System.err,scope)
+    ScriptableObject.putProperty(scope,"stderr",jsStderr)
+    // def jsSystem = Context.javaToJS(new System(),scope)
+    // ScriptableObject.putProperty(scope,"system",jsSystem)
 
-  /*ctx.evaluateReader(scope,
-    new InputStreamReader(this.getClass().getClassLoader()
-      .getResourceAsStream("src/resources/coffee-script.js")),
-    "coffee",0,null)*/
+    /*ctx.evaluateReader(scope,
+      new InputStreamReader(this.getClass().getClassLoader()
+        .getResourceAsStream("src/main/resources/coffee-script.js")),
+      "coffee",0,null)*/
 
-  // this.scope = scope
+    this.scope = scope
 
-  // Context.exit()
+    Context.exit()
+  }
+
+  Module(Scriptable scope) {
+    def ctx = Context.enter()
+    this.scope = ctx.newObject(scope)
+    this.scope.setParentScope(scope)
+    ctx.exit()
+  }
+
+  Module(Module module) {
+    this(module.scope)
+  }
+
+  void evaluateReader(String source, Reader reader) {
+    def ctx = Context.enter()
+    ctx.evaluateReader(scope,reader,source,0,null)
+    Context.exit()
+  }
+
+  void evaluateFile(String source) {
+    evaluateReader(source,new InputStreamReader(this.getClass().getClassLoader().getResourceAsStream(source)))
+
+    /*def ctx = Context.enter()
+
+    ctx.evaluateReader(scope,
+      new InputStreamReader(this.getClass().getClassLoader()
+        .getResourceAsStream(source)),
+      source,0,null)
+
+    Context.exit()*/
+  }
+
+  void evaluateString(String source) {
+    evaluateReader(null,new StringReader(source))
   }
 }
--- src/test/groovy/assets/test/assets.groovy
+++ src/test/groovy/assets/test/assets.groovy
@@ -1,11 +1,16 @@
 package assets.test
 
 import org.junit.*
+import org.mozilla.javascript.*
+import assets.*
 
 class Assets {
   @Test
   void module() {
-
+    def module = new Module()
+    module.evaluateString("this.count=4")
+    def count = module.scope.get("count")
+    Assert.assertEquals 4,count
   }
 
   @Test
