79d9f5a099b7c28305f9d6cb604e2baba343af04
David <ddk128@gmail.com>
2013-05-14 00:32:41 +0200

    Add beans to extension.

--- src/main/scala/assets/assets.scala
+++ src/main/scala/assets/assets.scala
@@ -1,6 +1,6 @@
 package assets
 
-import java.util.{List}
+import java.util.{List,LinkedHashMap}
 import scala.collection.JavaConversions._
 
 import org.gradle.api._
@@ -13,19 +13,31 @@ class Plugin extends org.gradle.api.Plugin[Project] {
 
   def apply(project:Project) {
     println("apply!")
-    project.getExtensions().create("assets",classOf[Extensions])
+    project.getExtensions().create("assets",classOf[Extension])
     project.task(Map("type"->classOf[Task]),"assets")
   }
 }
 
-class Extensions {
-  var optimize = false
-  var target = "assets"
+class Extension {
+  private var optimize = false
+  def setOptimize(value:Boolean) {this.optimize=value}
+  def getOptimize() = {this.optimize}
+
+  private var target = "assets"
+  def setTarget(value:String) {this.target=target}
+  def getTarget() = {this.target}
+
+  private var bundles:java.util.Map[String,Object]) = new LinkedHashMap[String,Object]()
+  def setBundles(bundles:Map[String,Object]) {this.bundles=bundles}
+  def getBundles():Map[String,Object]) = {this.bundles}
 }
 
 class Task extends DefaultTask {
   @TaskAction
   def compile() {
-    println("compile")
+    val extension = getProject().getExtensions().getByName("assets").asInstanceOf[Extension]
+    println("optimize: "+extension.getOptimize())
+    println("target: "+extension.getTarget())
+    println("bundles: "+extension.getBundles().size())
   }
 }
