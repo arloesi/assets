18f188c2e264b4bbd3347230a628becd105d95c3
David <ddk128@gmail.com>
2013-05-13 22:53:30 +0200

    Add build unit test.

--- src/test/scala/assets/test/assets.scala
+++ src/test/scala/assets/test/assets.scala
@@ -1,11 +1,13 @@
 package assets.test
 
-// import java.util._
+import java.io._
+import java.util.{List}
 import scala.collection.JavaConversions._
 
 import org.junit._
 import org.scalatest.junit._
-import org.gradle.testfixtures.ProjectBuilder
+import org.gradle.{GradleLauncher,StartParameter}
+import org.gradle.testfixtures._
 import org.gradle.api.Project
 import assets._
 
@@ -15,5 +17,13 @@ class Assets extends AssertionsForJUnit {
     val project = ProjectBuilder.builder().build()
     project.apply(Map("plugin"->"assets"))
     assert(project.getTasks().getByName("assets").isInstanceOf[Task])
+  }
+
+  @Test
+  def build() {
+    val params = new StartParameter()
+    params.setProjectDir(new File("src/test/resources"))
+    val launcher = GradleLauncher.newInstance(params)
+    launcher.run()
   }
 }
