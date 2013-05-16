d6235ee29586a0c86cbc0e908d53e4293b13dc1d
David <ddk128@gmail.com>
2013-05-16 15:04:13 +0200

    Add compilers.

--- src/main/scala/assets/assets.scala
+++ src/main/scala/assets/assets.scala
@@ -11,43 +11,89 @@ import org.gradle.api.{Project,DefaultTask,Task}
 import org.gradle.api.plugins._
 import org.gradle.api.tasks._
 
-class Plugin extends org.gradle.api.Plugin[Project] {
-  val coffee = new Coffee(new Context())
+object Assets {
+  import Compiler._
+
+  val context = new Context()
+  val coffee = new Coffee(context)
   val less = new Less()
+  val markup = new Markup(context,coffee)
+
+  class Extension {
+    private var optimize = false
+    def setOptimize(value:Boolean) {this.optimize=value}
+    def getOptimize() = {this.optimize}
+
+    private var target = "assets"
+    def setTarget(value:String) {this.target=target}
+    def getTarget() = {this.target}
+  }
+
+  class AssetsTask extends DefaultTask {
+    @TaskAction
+    def compile() {
+      val extension = getProject().getExtensions().getByName("assets").asInstanceOf[Extension]
+      println("optimize: "+extension.getOptimize())
+    }
+  }
+
+  class FileTask extends DefaultTask {
+    lazy val source = getInputs().getFiles().first
+    lazy val target = getOutputs().getFiles().first
+
+    @TaskAction
+    def compile()
+
+    def load() = {
+      IOUtils.toString(getClass().getClassLoader().getResourceAsStream(source.getPath()))
+    }
+
+    def save(source:String) {
+      FileUtils.write(target,source)
+    }
+  }
+
+  class CopyTask extends FileTask {
+    override def compile() {
+      FileUtils.copyFile(source, target)
+    }
+  }
+
+  class CoffeeTask extends FileTask {
+    override def compile() {
+      save(coffee.compile(source.getPath(),load()))
+    }
+  }
+
+  class LessTask extends FileTask {
+    override def compile() {
+      save(less.compile(source.getPath(),load()))
+    }
+  }
+}
+
+class Assets extends org.gradle.api.Plugin[Project] {
+  import Assets._
 
   def apply(project:Project) {
     project.getExtensions().create("assets",classOf[Extension])
     val config = new ConfigSlurper().parse(getClass().getClassLoader().getResource("assets.gradle"))
     val tasks = new LinkedList[Task]()
 
-    def parseImages(v:Object) = {
-      val tasks = new LinkedList[Task]()
-      val images = Bundle.loadImages(v.asInstanceOf[java.util.Map[String,Object]])
-
-      for((s,t) <- images) {
-        val task = project.task(Map("type"->classOf[ImageTask]),t)
-        task.getInputs().file(s)
-        task.getOutputs().file(t)
-        tasks.add(task)
-      }
-
-      tasks
-    }
-
-    config.getProperty("bundles") match {
-      case null => ()
-      case bundles:java.util.Map[String,Object] => {
-        for((k,v) <- bundles) {
-          tasks.addAll(parseImages(v))
-        }
-      }
-    }
-
     config.getProperty("modules") match {
       case null => ()
       case modules:java.util.Map[String,Object] => {
         for((k,v) <- modules) {
-          tasks.addAll(parseImages(v))
+          val bundle = new Bundle(k,v.asInstanceOf[java.util.Map[String,Object]])
+
+          buildImageTasks(project,bundle.images)
+          buildScriptTasks(project,bundle.scripts)
+          buildStyleTasks(project,bundle.styles)
+
+          // module
+          buildScriptTask(project,bundle)
+          buildStyleTask(project,bundle)
+          buildMarkupTask(project,bundle)
         }
       }
     }
@@ -55,29 +101,78 @@ class Plugin extends org.gradle.api.Plugin[Project] {
     val main = project.task(Map("type"->classOf[AssetsTask]),"assets")
     tasks.foreach(main.getDependsOn().add _)
   }
-}
 
-class Extension {
-  private var optimize = false
-  def setOptimize(value:Boolean) {this.optimize=value}
-  def getOptimize() = {this.optimize}
+  def buildImageTasks(project:Project,images:List[(String,String)]) = {
+    val tasks = new LinkedList[Task]()
 
-  private var target = "assets"
-  def setTarget(value:String) {this.target=target}
-  def getTarget() = {this.target}
-}
+    for((s,t) <- images) {
+      val task = buildCopyTask(project,s,t)
+      tasks.add(task)
+    }
 
-class AssetsTask extends DefaultTask {
-  @TaskAction
-  def compile() {
-    val extension = getProject().getExtensions().getByName("assets").asInstanceOf[Extension]
-    println("optimize: "+extension.getOptimize())
+    tasks
   }
-}
 
-class ImageTask extends DefaultTask {
-  @TaskAction
-  def compile() {
-    FileUtils.copyFile(getInputs().getFiles().first, getOutputs().getFiles().first)
+  def buildScriptTasks(project:Project,scripts:List[(String,String)]) = {
+    val tasks = new LinkedList[Task]()
+
+    for((s,t) <- scripts) {
+      val ext = FilenameUtils.getExtension(s)
+
+      if(ext == "js") {
+        tasks.add(buildCopyTask(project,s,t))
+      } else {
+        tasks.add(buildCoffeeTask(project,s,t))
+      }
+    }
+
+    tasks
+  }
+
+  def buildStyleTasks(project:Project,styles:List[(String,String)]) = {
+    val tasks = new LinkedList[Task]()
+
+    for((s,t) <- styles) {
+      val ext = FilenameUtils.getExtension(s)
+
+      if(ext == "css") {
+        tasks.add(buildCopyTask(project,s,t))
+      } else {
+        tasks.add(buildLessTask(project,s,t))
+      }
+    }
+
+    tasks
+  }
+
+  def buildFileTask(project:Project,`type`:Class[_],source:String,target:String) = {
+    val task = project.task(Map("type"->`type`),target)
+    task.getInputs().file(source)
+    task.getOutputs().file(target)
+    task
+  }
+
+  def buildCopyTask(project:Project,source:String,target:String) = {
+    buildFileTask(project,classOf[CopyTask],source,target)
+  }
+
+  def buildCoffeeTask(project:Project,source:String,target:String) = {
+    buildFileTask(project,classOf[CoffeeTask],source,target)
+  }
+
+  def buildLessTask(project:Project,source:String,target:String) = {
+    buildFileTask(project,classOf[LessTask],source,target)
+  }
+
+  def buildScriptTask(project:Project,bundle:Bundle) {
+
+  }
+
+  def buildStyleTask(project:Project,bundle:Bundle) {
+
+  }
+
+  def buildMarkupTask(project:Project,bundle:Bundle) {
+
   }
 }
--- src/main/scala/assets/compiler.scala
+++ src/main/scala/assets/compiler.scala
@@ -3,7 +3,44 @@ package assets
 import java.io._
 import java.util._
 import scala.collection.JavaConversions._
+import org.apache.commons.io._
+import org.mozilla.javascript._
+import org.lesscss.LessCompiler
 
-class Compiler {
-  def compile(name:String,source:String) = {source}
+object Compiler {
+  class Coffee(module:Context) {
+    module.evaluateFile("assets/coffee.js")
+
+    override def compile(name:String, source:String):String = {
+      val coffee = module.get("CoffeeScript")
+        .asInstanceOf[Scriptable]
+
+      val compile = coffee.get("compile",coffee)
+        .asInstanceOf[Function]
+
+      val ctx = Context.enter()
+      val js = compile.call(ctx, module.scope, coffee, Array(source))
+        .asInstanceOf[String]
+
+      Context.exit()
+
+      js
+    }
+  }
+
+  class Less {
+    val less = new LessCompiler()
+
+    override def compile(name:String,source:String):String = {
+      less.compile(source)
+    }
+  }
+
+  class Markup(context:Context,coffee:Coffee) extends Compiler {
+    context.evaluateString(coffee.compile("markup",IOUtils.toString(getClass().getClassLoader().getResourceAsStream("assets/markup.coffee"))))
+
+    override def compile(name:String,source:String):String = {
+      null
+    }
+  }
 }
